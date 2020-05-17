f1fd031486acfa790f457bb2ef84f42bd33a4177
Konstantin Perikov <pericov.ko@gmail.com>
2020-05-17 12:03:36 +0100

    add anomaly detector installation (not working atm(

---
+++ scripts/create_anomaly_detector.sh
@@ -0,0 +1,51 @@
+POST _opendistro/_anomaly_detection/detectors
+{
+  "name": "anomaly-test-detector",
+  "description": "Anomaly Test detector",
+  "time_field": "@timestamp",
+  "indices": [
+    "metricbeat*"
+  ],
+  "feature_attributes": [
+    {
+      "feature_name": "total_order",
+      "feature_enabled": true,
+      "aggregation_query": {
+        "total_order": {
+          "sum": {
+            "field": "system.process.cpu.total.value"
+          }
+        }
+      }
+    }
+  ],
+  "filter_query": {
+    "bool": {
+      "filter": [
+        {
+          "exists": {
+            "field": "system.process.cpu.total.value",
+            "boost": 1
+          }
+        }
+      ],
+      "adjust_pure_negative": true,
+      "boost": 1
+    }
+  },
+  "detection_interval": {
+    "period": {
+      "interval": 1,
+      "unit": "Minutes"
+    }
+  },
+  "window_delay": {
+    "period": {
+      "interval": 1,
+      "unit": "Minutes"
+    }
+  }
+}
+
+
+GET _opendistro/_anomaly_detection/detectors/results/_search
